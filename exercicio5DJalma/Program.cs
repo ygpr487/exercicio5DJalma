6fd4b1221ef292ba07a959e835bb6fca22d61ee9
fé <f@YAGO>
2022-06-30 21:47:03 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio5DJalma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio5DJalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("números pares entre 1 e 100\n");
+
+            for (n = 2; n < 100; n=n+2)
+            {
+                Console.WriteLine(n);
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
